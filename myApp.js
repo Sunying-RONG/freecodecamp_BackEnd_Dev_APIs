5da53c8c8489d82e25ebf51d520bfd0e4468bd51
Sunying RONG <sunying.rong@hotmail.com>
2022-01-30 14:35:25 +0100

    finish

--- myApp.js
+++ myApp.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 var express = require('express');
 var app = express();
+var bodyParser = require("body-parser");
 console.log('Hello World');
 
 // app.get('/', (req, res) => {
@@ -8,6 +9,16 @@ console.log('Hello World');
 // })
 
 console.log(__dirname);
+
+// --middleware-- for all routes
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(function(req, res, next) {
+    console.log(req.method + ' ' + req.path + ' - ' + req.ip);
+    // Call the next function in line:
+    next();
+  });
+// --middleware--
+
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
@@ -19,19 +30,36 @@ app.use('/public', express.static(__dirname + '/public'));
 if (process.env.MESSAGE_STYLE === "uppercase") {
     response = "Hello World".toUpperCase();
 } else {
-    response = "Hello World";
+    response = "Hello World"; // JSON must ""
 }
-const message = {"message": response}; // must ""
+const message = {message: response}; 
 app.get('/json', (req, res) => {
   res.json(message);
 })
 
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.send({ time : req.time });
+})
 
+app.get('/:word/echo', (req, res) => {
+    console.log(req.params);
+    const { word } = req.params;
+    console.log(word);
+    res.json({ echo : word });
+})
 
+app.get('/name', (req, res) => {
+    const { first, last } = req.query;
+    res.json( { name : `${first} ${last}` });
+})
 
-
-
-
+app.post('/name', (req, res) => {
+    const { first, last } = req.body;
+    res.json( { name : `${first} ${last}` });
+})
 
 
 
